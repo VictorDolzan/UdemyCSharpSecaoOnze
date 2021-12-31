4a067830755536026ab4fc4b5f5be7215d0297f4
Victor Dolsan <victorddf5@gmail.com>
2021-12-31 11:48:27 -0300

    Inicializando o repositorio da secao 11 do curso de csharp da Udemy

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace CSharpSecaoOnze
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Iniciando a seção 11 do Curso e C# da Udemy!");
+        }
+    }
+}
